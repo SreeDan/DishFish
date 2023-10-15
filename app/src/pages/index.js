01a682789f0951d8212ea196feff1b28b6942306
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2023-10-14 22:59:41 -0400

    added user role auth

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,23 +1,34 @@
 import Image from 'next/image'
-import { Text, Button, Link, VStack } from '@chakra-ui/react'
+import { Layout } from '../components/Layout';
+import { Text, Button, Link, VStack, Container } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <div>
-      <VStack>
-        <Text color="white" fontSize='6xl'>
+    <>
+    <Layout />
+      <Container background={"#008080"} maxW={'max-content'}>
+      <VStack spacing='5'>
+        <Text fontWeight={"bold"} padding='10px' color="white" fontSize='6xl'>
           DishFish
         </Text>
-        <Text color="white" fontSize='xl'>
+        <Text color="white" fontSize='xl' margin={"1em"} paddingBottom={"10"}>
           Simplifying dining by helping users find their perfect dish through AI within budget and dietary preferences while empowering restaurants with data-driven insights and dynamic pricing.
         </Text>
+
+        <Link>
+          <Button>
+            Sign Up
+          </Button>
+        </Link>
       </VStack>
-      <Button>
-        <Link>Sign In</Link>
-      </Button>
-      <Button>
-        <Link>Sign Up</Link>
-      </Button>
-    </div>
+    </Container>
+      <Link>
+        <Button>Sign In</Button>
+      </Link>
+      
+      <Link>
+        <Button>Sign Out</Button>
+      </Link>
+    </>
   )
 }
